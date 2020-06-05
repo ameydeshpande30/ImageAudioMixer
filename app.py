63a8cd18d4ff893d7fe4c5105e7fc38492f492bc
Amey Deshpande <ameydeshpande30@gmail.com>
2020-06-05 19:30:24 +0530

    update ui

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, jsonify, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
 import os, shutil, threading
 from datetime import datetime
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder="static")
 inputFilePath = "inputs/"
 outputFilePath = "outputs/"
+WAITING_SECONDS = 10
 app.config['UPLOAD_FOLDER'] = inputFilePath
 data = {}
 def clenUP():
+    global data
+    data = {}
     try:
         shutil.rmtree(outputFilePath)
     except:
@@ -35,6 +38,7 @@ def mergeData(inputImage, inputAudio, outputfilename, event):
     cmd = "ffmpeg -loop 1 -i {} -i {} -c:v libx264 -tune stillimage -strict experimental -c:a aac -b:a 192k -pix_fmt yuv420p  -shortest {}".format(inputImage, inputAudio, outputfilename)
     os.system(cmd)
     event.set()
+    print("Event is set")
 
 def start_func(inputImage, inputAudio, outputfilename):
     event = threading.Event()
@@ -42,9 +46,14 @@ def start_func(inputImage, inputAudio, outputfilename):
     t1.start()
     return event
 
-@app.route('/')
-def upload_file():
-   return render_template('upload.html')
+@app.route("/test")
+def testPage():
+    return render_template('download.html')
+
+
+# @app.route('/')
+# def upload_file():
+#    return render_template('upload.html')
 	
 @app.route('/', methods = ['GET', 'POST'])
 def uploader_file():
@@ -62,19 +71,35 @@ def uploader_file():
         # return 'file uploaded successfully' + outputCode
         return redirect ("/wait/" + outputCode)
     else:
-        return render_template('upload.html')
+        return render_template('home.html')
+
+@app.route("/downloadFile/<code>", methods=['GET'])
+def sendFileVideo(code):
+    code = str(code) + ".mp4"
+    try:
+        return send_from_directory(outputFilePath, filename=code, as_attachment=True)
+    except :
+        return render_template('error.html') 
+
 
 @app.route("/wait/<code>")
 def waitTime(code):
     try:
         event = data[code]
+        print(event)
         if event.isSet():
-            return "done"
+            return redirect ("/downloadPage/" + str(code))
         else:
-            return "Please refresh again to see"
+            return render_template('waiting.html', WAITING_SECONDS=WAITING_SECONDS)
 
     except:
-        return 'Not Found'
+        return render_template('error.html')
+
+@app.route("/downloadPage/<code>")
+def downloadPage(code):
+    link = "/downloadFile/" + str(code)
+    return render_template('download.html', link=link)
+
 
 
 @app.route("/api")
